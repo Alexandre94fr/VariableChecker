b7f9df378dda6e3fe2f5b69ab6f9942ac54459ce
Alexandre <Alexandre94.fr@gmail.com>
2025-05-24 11:27:07 +0200

    🔨 ▸ Adding VariableChecker tool

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# VariableChecker
+# Instantiator
+
+## ➤ Creation context :
+During several group game development projects on the Unity engine, I noticed that each team member implemented singletons in their own way, often with inconsistent safety checks and conflict management strategies.
+
+To speed up our workflow and avoid redundant boilerplate code, I decided to create a centralized tool to standardize the singleton pattern across projects.
+
+Exactly one year later, I'm sharing this tool with you for free and open-source use.
+
+
+## ➤ Description :
+### ▸ What is a Singleton ?
+
+A singleton is a software design pattern that ensures a class has **only one instance** and provides a **global point of access to it**. It’s commonly used in Unity for manager-like components, such as audio managers, game managers _(please avoid putting everything into a giant GameManager class)_, and more, where a single authoritative instance is needed (across scenes or not).
+
+### ▸ What is Instantiator ?
+
+Instantiator is a static Unity utility script that simplifies the process of implementing singleton patterns in your game objects. It automates :
+
+- Detection of existing instances.
+- Conflict resolution (with customizable behaviors like warnings, automatic destruction, or editor pause).
+- Centralized naming convention enforcement for singleton variables (public static TYPE Instance).
+
+By using Instantiator.GetInstance(...), you can write safer, more maintainable, and faster singleton implementations, especially in collaborative projects.
+
+No need to write and rewrite the same boilerplate for every class. Just plug and play.
+
+**Here’s are some typical examples of singleton creation, with and without the Instantiator:**
+
+![InstantiatorUseExample_FromTo_RawImage1](https://github.com/user-attachments/assets/b77cd1c2-7f61-48ed-a113-33231d8ae35f)
+![InstantiatorUseExample_FromTo_RawImage2](https://github.com/user-attachments/assets/db2e6a17-8064-4725-a8dc-fb621c260b45)
+
+
+## ➤ Utilization examples :
+
+Here’s a typical example of how to use the Instantiator tool in a singleton class :
+
+![InstantiatorUseExample](https://github.com/user-attachments/assets/23f1f22c-1698-4b6f-9762-3860a2d26037)
+ 
+As you can see in the first example we use reflexion to get the 'Instance' variable, if needed you can change the targeted variable by going in the Instantiator and changing the DEFAULT_SINGLETON_VARIABLE_NAME variable's value.
+
+**Note :** If you need more documentation check out the Instantiator class.
+
+
+## ➤ Installation :
+
+In reality there is no need of downloading the projet, I made this tool compatible with the Unity's package manager.
+
+In order to install the tool, follow this steps :
+
+- Open the Unity's package manager
+
+At the **top of the Unity application** you will see multiple dropdown, please **click on the 'Window'** one 
+
+![image](https://github.com/user-attachments/assets/826603ad-3a94-437c-9e41-09537c40cf5a)
+
+After that **click on the 'Package Manager' button**
+
+![image](https://github.com/user-attachments/assets/bb4905eb-d86c-47dd-9da3-ef81e274f8b5)
+
+- Add the package
+
+Now that the Package Manager is open, click on the '+' dropdown
+
+![image](https://github.com/user-attachments/assets/ab62972c-8340-4f3a-9b13-ce8ed35d1de1)
+
+After that **click on the 'Add package from git URL...' button**
+
+![image](https://github.com/user-attachments/assets/125918bd-e34d-41b5-96ee-49f6dcdb42e7)
+
+Copy (Ctrl C) and paste (Ctrl V) the project link [https://github.com/Alexandre94fr/Instantiator.git] into this :
+
+![image](https://github.com/user-attachments/assets/1a486ff7-0708-4faf-9e59-4194044a2fa7)
+
+And finally **press the 'Add' button**
+
+![image](https://github.com/user-attachments/assets/328133d4-0fd5-473d-aff9-93912227f065)
+
+Good job, now you can use the Instantiator, have fun ^^
+
+
+## ➤ License :
+
+As mentioned earlier, you're free to use Instantiator in your projects. 
+
+If you release a game using this tool, I’d appreciate a small credit, for example, as “External Technical Help”.
+
+Thank you ! :D
+
+
+## ➤ Credits :
+
+### ▸ Programmers :
+- [Alexandre RICHARD](https://github.com/Alexandre94fr)
+
+### ▸ External assets
+- No external assets used
---
+++ Runtime/VariableChecker.cs
@@ -0,0 +1,189 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VariableCheckerPackage
+{
+    public static class VariablesChecker
+    {
+        /// <summary>
+        /// A list of checks that will check if the given variable triggers verifications
+        /// </summary>
+        readonly static List<Func<string, (object variable, string variableName), bool>> DEFAULT_VARIABLE_CHECKS = new()
+        {
+            IsVariableNullCheck,
+        };
+
+        #region -= Checks =-
+
+        public static bool IsVariableNullCheck(string p_gameObjectName, (object variable, string variableName) p_variableToCheck)
+        {
+            if (p_variableToCheck.variable == null || (p_variableToCheck.variable is UnityEngine.Object unityObject && unityObject == null))
+            {
+                Debug.LogError(
+                    $"ERROR ! The variable '{p_variableToCheck.variableName}' in '{p_gameObjectName}' GameObject is null," +
+                    $" please set it through the Unity inspector, or directly at the variable initialization."
+                );
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsNumberVariableUnderZeroCheck(string p_gameObjectName, (object variable, string variableName) p_variableToCheck)
+        {
+            if ((float)p_variableToCheck.variable < 0)
+            {
+                Debug.LogError(
+                    $"ERROR ! The variable '{p_variableToCheck.variableName}' in '{p_gameObjectName}' GameObject is under zero."
+                );
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsNumberVariableEqualZeroCheck(string p_gameObjectName, (object variable, string variableName) p_variableToCheck)
+        {
+            if ((float)p_variableToCheck.variable == 0)
+            {
+                Debug.LogError(
+                    $"ERROR ! The variable '{p_variableToCheck.variableName}' in '{p_gameObjectName}' GameObject equals zero."
+                );
+                return true;
+            }
+
+            return false;
+        }
+
+        // You can add more checks here.
+        #endregion
+
+        /// <summary>
+        /// Returns if the given variables are correcly setted (by default it's only checking if it's null).
+        ///  
+        /// <para> ------------------- </para>
+        /// 
+        /// <para> <b> WARNING : </b> The variable passed are copied, that means that if you past a very big variable this method call will cause lag due to memory management. </para>
+        /// 
+        /// <para> Sadly we can't pass your variables as references because the keyword 'params' before it is incompatible. </para>
+        /// 
+        /// <para> However you can use instead the <see cref = "IsVariableCorrectlySetted"/> method. </para>
+        /// 
+        /// <para> ------------------- </para>
+        /// 
+        /// <para> <b> Code utilization example n°1 : </b> <code>
+        /// 
+        /// void Start()
+        /// {
+        ///     if (!VariablesChecker.AreVariablesCorrectlySetted(name, null,
+        ///         (_exampleVariable1, nameof(_exampleVariable1)),
+        ///         (_exampleVariable2, nameof(_exampleVariable2)),
+        ///         (_exampleVariable3, nameof(_exampleVariable3))
+        ///     )) return;
+        /// }
+        /// </code> </para> 
+        /// 
+        /// <para> <b> Code utilization example n°2 : </b> </para>
+        /// <para> <b> NOTE : </b> Due to a conflict with the XML system (used for code documentation) and the example code below, the 'IN angle brackets, OUT angle brackets' will be replaced by '/ \'. 
+        /// But if you see '// Thing' it's just a comment. 
+        /// 
+        /// <code>
+        /// 
+        /// int _exampleVariable1 = 42;
+        /// string _exampleVariable2 = "Hello world";
+        /// bool _exampleVariable3 = true;
+        /// 
+        /// static readonly List/Func/string, (object variable, string variableName), bool\\ CUSTOM_VARIABLE_CHECKS = new()
+        /// {
+        ///     VariablesChecker.IsVariableNullCheck,
+        ///     IsVariableTestCheck
+        ///     // You can add as many methods as you like
+        /// };
+        /// 
+        /// // This method should be 'static'
+        /// static bool IsVariableTestCheck(string p_gameObjectName, (object variable, string variableName) p_variableToCheck)
+        /// {
+        ///     Debug.Log("I'm always returning false for demonstration purpose.");
+        /// 
+        ///     return false;
+        /// }
+        /// 
+        /// void Start()
+        /// {
+        ///     VariablesChecker.AreVariablesCorrectlySetted(name, CUSTOM_VARIABLE_CHECKS,
+        ///         (_exampleVariable1, nameof(_exampleVariable1)),
+        ///         (_exampleVariable2, nameof(_exampleVariable2)),
+        ///         (_exampleVariable3, nameof(_exampleVariable3))
+        ///     );
+        /// }
+        /// </code> </para> </summary>
+        /// <param name = "p_variableOwnerName"> The variable owner name, can be for example the GameObject name, or the Class name. </param>
+        /// <param name = "p_customVariableChecks"> A list of functions (methods that returns something), 
+        /// that list of functions correspond to all the checks that the given variable will pass throw, to use default ones just pass 'null'.
+        /// You can see the default ones in <see cref = "DEFAULT_VARIABLE_CHECKS"/> </param>
+        /// <param name = "p_variablesToCheck"> A params of tuple (an object and a string) </param>
+        public static bool AreVariablesCorrectlySetted(string p_variableOwnerName,
+            List<Func<string, (object variable, string variableName), bool>> p_customVariableChecks = null,
+            params (object variable, string variableName)[] p_variablesToCheck)
+        {
+            bool areVariablesCorreclySetted = true;
+
+            List<Func<string, (object variable, string variableName), bool>> variableChecks = DEFAULT_VARIABLE_CHECKS;
+
+            // Checking if the user gave customs variable checks
+            if (p_customVariableChecks != null)
+            {
+                variableChecks = p_customVariableChecks;
+            }
+
+            // Looping throw each given variables
+            for (int i = 0; i < p_variablesToCheck.Length; i++)
+            {
+                // Doing every checks
+                for (int j = 0; j < variableChecks.Count; j++)
+                {
+                    if (variableChecks[j](p_variableOwnerName, p_variablesToCheck[i]))
+                    {
+                        areVariablesCorreclySetted = false;
+                    }
+                }
+            }
+
+            return areVariablesCorreclySetted;
+        }
+
+        /// <summary>
+        /// <b> WARNING ! </b> Not fully usable for now.
+        /// <para> Please use '<see cref="AreVariablesCorrectlySetted"/>'. </para>
+        /// </summary>
+        /// <param name = "p_gameObjectName">  </param>
+        /// <param name = "p_variable">  </param>
+        /// <param name = "p_variableName">  </param>
+        /// <param name = "p_customVariableChecks">  </param>
+        [Obsolete]
+        public static bool IsVariableCorrectlySetted(string p_gameObjectName, ref object p_variable, string p_variableName,
+            List<Func<string, (object variable, string variableName), bool>> p_customVariableChecks)
+        {
+            bool areVariablesCorreclySetted = true;
+
+            List<Func<string, (object variable, string variableName), bool>> variableChecks = DEFAULT_VARIABLE_CHECKS;
+
+            // Checking if the user gave customs variable checks
+            if (p_customVariableChecks != null)
+            {
+                variableChecks = p_customVariableChecks;
+            }
+
+            for (int i = 0; i < variableChecks.Count; i++)
+            {
+                if (variableChecks[i](p_gameObjectName, (p_variable, p_variableName)))
+                {
+                    areVariablesCorreclySetted = false;
+                }
+            }
+
+            return areVariablesCorreclySetted;
+        }
+    }
+}
